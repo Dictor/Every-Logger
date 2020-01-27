9743b033efdc53fbd54d7891d113dd201089a1c0
Dictor <kimdictor@gmail.com>
2020-01-27 14:51:58 +0000

    Refactoring Hub
    * Rename objects
    * Abstract repeated using code to function

--- hub.go
+++ hub.go
@@ -1,71 +1,77 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Original Code by : Copyright C) 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Modified by Dictor(kimdictor@gmail.com)
 
 package main
 
-import (
-	"log"
+type WebsocketEventKind int
+
+const (
+	EVENT_RECIEVE WebsocketEventKind = iota
+	EVENT_REGISTER
+	EVENT_UNREGISTER
+	EVENT_BROADCAST
 )
 
-type RecieveEvent struct {
+type WebsocketEvent struct {
+	kind   WebsocketEventKind
 	client *Client
 	msg    *[]byte
 }
 
-// Hub maintains the set of active clients and broadcasts messages to the
-// clients.
-type Hub struct {
-	// Registered clients.
-	clients map[*Client]bool
-
-	// Inbound messages from the clients.
-	broadcast chan []byte
-
-	recieve chan *RecieveEvent
-
-	// Register requests from the clients.
-	register chan *Client
-
-	// Unregister requests from clients.
+type WebsocketHub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	recieve    chan *WebsocketEvent
+	register   chan *Client
 	unregister chan *Client
 }
 
-func newHub() *Hub {
-	return &Hub{
+func newWebsocketHub() *WebsocketHub {
+	return &WebsocketHub{
 		broadcast:  make(chan []byte),
-		recieve:    make(chan *RecieveEvent),
+		recieve:    make(chan *WebsocketEvent),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
 	}
 }
 
-func (h *Hub) run(recv_cb func(*RecieveEvent)) {
+func (h *WebsocketHub) run(event_callback func(*WebsocketEvent)) {
 	for {
 		select {
 		case client := <-h.register:
-			log.Println("[WS REG]", makeWsPrefix(client))
 			h.clients[client] = true
+			event_callback(&WebsocketEvent{EVENT_REGISTER, client, nil})
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
-				log.Println("[WS UNREG]", makeWsPrefix(client))
-				delete(h.clients, client)
-				close(client.send)
+				h.closeClient(client)
+				event_callback(&WebsocketEvent{EVENT_UNREGISTER, client, nil})
 			}
 		case message := <-h.broadcast:
 			for client := range h.clients {
-				select {
-				case client.send <- message:
-					log.Printf("[WS BROAD]\n")
-				default: // when client.send closed, close and delete client
-					log.Println("[WS UNREG]", makeWsPrefix(client))
-					close(client.send)
-					delete(h.clients, client)
+				if h.sendSafe(client, &message) {
+					event_callback(&WebsocketEvent{EVENT_BROADCAST, client, nil})
+				} else {
+					event_callback(&WebsocketEvent{EVENT_UNREGISTER, client, nil})
 				}
 			}
-		case recv := <-h.recieve:
-			recv_cb(recv)
+		case evt := <-h.recieve:
+			event_callback(evt)
 		}
+	}
+}
+
+func (h *WebsocketHub) closeClient(cli *Client) {
+	close(cli.send)
+	delete(h.clients, cli)
+}
+
+func (h *WebsocketHub) sendSafe(cli *Client, msg *[]byte) bool {
+	select {
+	case cli.send <- *msg:
+		return true
+	default: // when client.send closed, close and delete client
+		h.closeClient(cli)
+		return false
 	}
 }
