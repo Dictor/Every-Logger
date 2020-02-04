f475568fde377304a6bf3b3bcdeeca4dbda2b2b7
Dictor <kimdictor@gmail.com>
2020-02-04 14:22:04 +0000

    Implement every function which is wrote pure js to  vuejs

--- front/app-vue.js
+++ front/app-vue.js
@@ -7,26 +7,79 @@ var Model = {
     ValueDelta: 0.0,
     RecievedDateDelta: 0.0,
     RecievedDate: 0,
-    Chart: null
+    ErrorMsg: "",
+    Chart: null,
+    GetValueHistory: async function(topic_name, data_term) {
+        var data = await RequestXhrGetPromise("ival?topic=" + topic_name + "&term=" + data_term);
+        return JSON.parse(data);
+    }
 };
 
-//
 var View = {
     InfoContent: null,
     Init: function(topic) {
         this.InfoContent = new Vue({
-            el: "#info-content",
+            el: "#info",
             data: Model
         });
         
+        if (!topic || topic == "") {
+            Model.ErrorMsg = "Invalid topic name.";
+        }
+        
         Ws.Init();
-        Ws.conn.onopen = function(evt) {
+        Ws.conn.onopen = async function(evt) {
             Ws.Send("TOPIC,"+topic);
+            var history = await Model.GetValueHistory(topic, "1m");
+            View.InitChart(history);
+            Model.ValueLastTerm = Number(history[history.length - 2][1]);
         }
         
         setInterval(function() {
                 Model.RecievedDateDelta = (Date.now() - Model.RecievedDate) / 1000;
-            }, 100);
+        }, 100);
+    },
+    InitChart: function(ivalue) {
+        Highcharts.setOptions({
+            time: {
+                timezone: 'Asia/Seoul'
+            }
+        });
+        Model.Chart = Highcharts.chart('container', {
+            chart: {
+                type: 'spline',
+                backgroundColor: '#1C1D21'
+            },
+            title: {
+                text: Model.TopicName,
+                style: {color: '#D9D5C1'}
+            },
+            xAxis: {
+                type: 'datetime',
+                title: {
+                    text: '시간'
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true
+                    },
+                    enableMouseTracking: false
+                },
+                series: {
+                    color: '#2ECC71'
+                }
+            },
+            tooltip: {
+                headerFormat: '<b>' + Model.TopicName + '</b><br>',
+                pointFormat: '{point.x:%Y/%m/%d %H:%M:%S} : <b>{point.y:.2f}</b>'
+            },
+            series: [{
+                name: Model.TopicName,
+                data: ivalue
+            }]
+        });
     }
 };
 
@@ -35,7 +88,7 @@ var Ws = {
     Init: function() {
         this.conn = new WebSocket("ws://" + document.location.host + "/ws");
         this.conn.onclose = function (evt) {
-            //UI.showError("Websocket closed, Please refresh this page.");
+            Model.ErrorMsg = "Websocket closed, Please refresh this page.";
         };
         this.conn.onmessage = function (evt) {
             var pstr = evt.data.split(",");
@@ -43,7 +96,7 @@ var Ws = {
                 case 2:
                     switch (pstr[0]) {
                         case "ERROR":
-                            //UI.showError(pstr[1]);
+                            Model.ErrorMsg = pstr[1];
                             break;
                     }
                 case 3:
@@ -58,8 +111,7 @@ var Ws = {
                         case "VALUE":
                             Model.Value = Number(pstr[3]);
                             Model.ValueDate = Number(pstr[2]);
-                            Model.ValueDelta = (Model.ValueLastTerm - Model.Value) / Model.ValueLastTerm * 100;
-                            Model.ValueLastTerm = Model.Value;
+                            Model.ValueDelta = (Model.Value - Model.ValueLastTerm) / Model.ValueLastTerm * 100;
                             Model.RecievedDate = Date.now();
                             break;
                     }
